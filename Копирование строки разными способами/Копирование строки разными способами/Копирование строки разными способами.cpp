627c42a83ead57d515555c21409161227ba3785b
Vladislav Tsybulya <vladmir909@mail.ru>
2018-12-07 02:52:00 +0800

    Добавил "Копирование строки разными способами"

---
+++ Копирование строки разными способами/Копирование строки разными способами/Копирование строки разными способами.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char src[] = "Some string";
+	char dst[20];
+	// Копирование через индексацию
+	int i = 0;
+	for (i = 0; src[i] != 0; i++)
+		dst[i] = src[i];
+	dst[i] = 0;
+	cout << "Копирование через индексацию: " << dst << endl;
+	// Копирование через указатели I
+	const char* psrc = src;
+	char* pdst = dst;
+	while (*psrc)
+		*pdst++ = *psrc++;
+	*pdst = 0;
+	cout << "Копирование через указатели I: " << dst << endl;
+	// Копирование через указатели II
+	psrc = src;
+	pdst = dst;
+	while (*pdst++ = *psrc++);
+	cout << "Копирование через указатели II: " << dst << endl;
+	return 0;
+}
